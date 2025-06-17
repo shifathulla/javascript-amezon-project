41cf7380d132a463b030b3b1a28ab515f153af12
shifathulla <shifathullaz1342@gmail.com>
2025-06-17 22:12:27 +0530

    Add files via upload

---
+++ scripts/products.js
@@ -0,0 +1,33 @@
+const products =[{
+  image:'images/products/athletic-cotton-socks-6-pairs.jpg',
+  name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
+  rating: {
+    stars:4.5,
+    counts:87
+  },
+  price: 1090
+},{
+  image:'images/products/intermediate-composite-basketball.jpg',
+  name:'Intermediate Size Basketball',
+  rating:{
+    stars:4.5,
+    counts:127
+  },
+  price: 2095
+},{
+  image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+  name:'Adults Plain Cotton T-Shirt - 2 Pack',
+  rating:{
+    stars:4.5,
+    counts:56
+  },
+  price: 799
+},{
+  image: 'images/products/Oppo-Reno8-T-5G.png',
+  name:'Oppo-Reno8-T-5G',
+  rating:{
+    stars:5,
+    counts:34
+  },
+  price: 39999
+}];
